da8c876512cae5fe2059168fef89f05edccc0705
davchos <dchosrova@gmail.com>
2021-07-18 19:41:39 +0200

    Correct Typo in cors declaration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const formidable = require("express-formidable");
 
-const cosrs = require("cors");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 
